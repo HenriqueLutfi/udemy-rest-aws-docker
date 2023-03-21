3e22934abf82c4d46399e9c890c23d370d738a58
HenriqueLutfi <henrique_lutfi@hotmail.com>
2023-03-21 10:51:29 -0300

    first step

---
+++ udemy-rest-aws-docker/src/main/java/br/com/lutfi/UdemyRestAwsDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lutfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRestAwsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRestAwsDockerApplication.class, args);
+	}
+
+}
